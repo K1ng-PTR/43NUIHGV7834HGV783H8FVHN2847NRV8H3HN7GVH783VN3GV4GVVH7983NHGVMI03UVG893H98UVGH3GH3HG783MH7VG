e007152c5b7835cd99fd2c494ce23cb38c428fbd
K1ng-PTR <gusbrasil09@gmail.com>
2025-02-13 23:53:50 -0100

    Update API.py

--- API.py
+++ API.py
@@ -274,8 +274,7 @@ def sucesso():
         <p>Tipo de compra: <strong>{detalhes["tipo"]}</strong></p>
         <p>Sua chave de licença:</p>
         <div class="key">{chave}</div>
-        <!-- O style abaixo força a quebra de linha para o ID da compra -->
-        <p style="word-break: break-all;">ID da Compra: {id_compra}</p>
+        <p style="word-break: break-all; text-align: center;">ID da Compra: {id_compra}</p>
         <p>{ "Validade: " + detalhes["expire_at"] if detalhes["expire_at"] else "Sem expiração" }</p>
       </div>
     </body>
